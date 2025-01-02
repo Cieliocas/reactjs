11edd38ca3fe7026210bafba10edb1de2baecc1e
Cieliocas <francielioevangelista@hotmail.com>
2025-01-02 06:12:24 -0300

    add function to return name and display it in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 function App() {
 	const nome = "Cielio"
+
+	function retornaNome() {
+		return nome
+	}
+
+	return(
+		<div>
+			<h1>Olá {retornaNome()}</h1>
+			<h2>Subtítulo</h2>
+		</div>
+	)
 }
 export default App
