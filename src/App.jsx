29ce8ac0089bca98ed313ddc835c90cff5f46285
Cieliocas <francielioevangelista@hotmail.com>
2025-01-02 06:15:08 -0300

    add person object and conditional greeting to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ function App() {
 		return nome
 	}
 
+	const pessoa = {
+		nome: "Cieliocas",
+		idade: 25,
+	}
+
+	let estaDeNoite = false;
+
 	return(
 		<>
 			<h1>Olá {retornaNome()}</h1>
+			<h1>Olá {pessoa.nome}</h1>
 			<h2>Subtítulo</h2>
+			{estaDeNoite ? 'Boa noite' : 'Bom dia'}
 		</>
 	)
 }
