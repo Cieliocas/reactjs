6b4a65d4a97f2bab75fd722c49079b6438e03527
Cieliocas <francielioevangelista@hotmail.com>
2025-01-04 03:00:25 -0300

    fix: update class attributes to className in Perfil component

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -8,8 +8,8 @@ const Perfil = () => {
     
     return (
             <div>
-                <img class="perfil-avatar" src={usuario.avatar} />
-                <h3>{usuario.nome}</h3>
+                <img className="perfil-avatar" src={usuario.avatar} />
+                <h3 className="perfil-titulo" >{usuario.nome}</h3>
             </div>
     )
 }
