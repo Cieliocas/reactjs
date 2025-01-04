fc6db5aa88129609e6e4957a50fe357a634c5b94
Cieliocas <francielioevangelista@hotmail.com>
2025-01-04 05:08:57 -0300

    feat: enhance ReposList component with loading state and detailed repository display

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from "react";
 
 const ReposList = () => {
-    const [respos, setRepos] = useState([]);
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://api.github.com/users/cieliocas/repos')
         .then(res => res.json())
         .then(resJson => {
-            setRepos(resJson);
-        
+            setTimeout(() => {
+                setLoading(false);
+                setRepos(resJson);
+            }, 3000); 
         })
     }, []);
 
     return (
-        <ul>
-            <li>Repositório</li>
-        </ul>
+        <>
+            {loading && <h1>Carregando...</h1>}
+            <ul>
+                {repos.map(repositorio => (
+                    <li key={repositorio.id}>
+                        <b>Nome: </b> {repositorio.name} <br />
+                        <b>Linguagem: </b> {repositorio.language} <br />
+                        <a target="_blank" href={repositorio.html_url}>Visitar no Github</a> <br />
+                    </li>
+                ))}
+                <li>Repositório</li>
+            </ul>
+        </>
     );
 }
 
